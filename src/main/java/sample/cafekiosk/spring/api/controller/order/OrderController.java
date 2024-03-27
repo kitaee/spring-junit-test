e791921a39c34020c0c156170ef665c24af76c5a
kitae <kitae_lee3@tmax.co.kr>
2024-03-27 09:30:53 +0900

    feat: Business Layer 테스트(2)

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.order.OrderService;
 import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +17,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public void createOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
-        orderService.createOrder(orderCreateRequest, LocalDateTime.now());
+    public OrderResponse createOrder(@RequestBody OrderCreateRequest orderCreateRequest) {
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+        return orderService.createOrder(orderCreateRequest, registeredDateTime);
     }
 }
--- src/main/java/sample/cafekiosk/spring/api/service/order/request/OrderCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/request/OrderCreateRequest.java
@@ -2,10 +2,12 @@ package sample.cafekiosk.spring.api.service.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
 
     public List<String> productNumbers;
